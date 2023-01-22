e498e4b09deb5df73ef313a9f23cee142ddfcb8d
veskovelichkov <veselin.velichkov00@gmail.com>
2023-01-22 14:24:13 +0100

    Created the game Rock,Paper,Scissors

---
+++ rock_paper_scissors.py
@@ -0,0 +1,27 @@
+import random
+rock='Rock'
+paper='Paper'
+scissors='Scissors'
+player_move=input("Choose [r]ock, [p]aper, [s]cissors: ")
+if player_move=='r':
+    player_move=rock
+elif player_move=='p':
+    player_move=paper
+elif player_move=='s':
+    player_move=scissors
+else:
+    raise SystemExit("Invalid input. Try again...")
+computer_random_number=random.randint(1,3)
+computer_move=''
+if computer_random_number==1:
+    computer_move=rock
+elif computer_random_number==2:
+    computer_move=paper
+else:
+    computer_move=scissors
+if (player_move==rock and computer_move==scissors) or \
+        (player_move==scissors and computer_move==paper) or \
+        (player_move==paper and computer_move==rock):
+    print('Congratulations, you won!')
+else:
+    print('You lost...')
